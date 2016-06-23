03a2876ca97bf57bc1ecfb6751b1e5615c146c23
26247 <26247@protonmail.com>
2016-06-23 10:57:15 -0700

    i have no idea

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # js
 I want to make a cool page.  
-This is a test repo of MEAN stack.  
+This is a test repo of MEAN stack + Socket.IO.  
 Donation(bitcoin): 1NiR6ioFjR9G5ePP8DPxH1uz4teaud5LsH
--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res)
 {
-    res.send('Hello World');
+    res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(26247, function()
+nameNumber = 0;
+
+io.on('connection', function(socket)
+{
+    socket.on('identity', function(msg)
+    {
+        io.emit('identity', nameNumber);
+        nameNumber += 1;
+    });
+    socket.on('tempMessage', function(msg)
+    {
+        realName = msg.substr(0, msg.indexOf("\n"));
+        realMsg = msg.substr(msg.indexOf("\n") + 1);
+        io.emit('tempMessage', realName + "\n" + realMsg);
+    });
+});
+
+http.listen(26247, function()
 {
     console.log('On port 26247');
 });

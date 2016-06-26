edadbeaab7c2ea4637558556258eacd025d50365
26247 <26247@protonmail.com>
2016-06-26 09:42:58 +0900

    redis

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # js
 I want to make a cool page.  
-This is a test repo of Nodejs, Socket.IO and Expressjs.  
+This is a test repo of Nodejs, Socket.IO, Expressjs and Redis.  
 Donation(bitcoin): 1NiR6ioFjR9G5ePP8DPxH1uz4teaud5LsH
--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var redis = require('redis')
+var client = redis.createClient();
+
+client.on("error", function(err)
+{
+    console.log("Error: " + err);
+});
 
 app.get('/', function(req, res)
 {
@@ -21,6 +28,17 @@ io.on('connection', function(socket)
         realName = msg.substr(0, msg.indexOf("\n"));
         realMsg = msg.substr(msg.indexOf("\n") + 1);
         io.emit('tempMessage', realName + "\n" + realMsg);
+        client.set("message" + realName, realName + "\n" + realMsg);
+    });
+    socket.on('getAllMessage', function(msg)
+    {
+        for(var i = nameNumber - 1; i >= 0 && i >= nameNumber - 101; i -= 1)
+        {
+            client.get("message" + i, function(err, reply)
+            {
+                io.emit('tempMessage', reply);
+            });
+        }
     });
 });
 
